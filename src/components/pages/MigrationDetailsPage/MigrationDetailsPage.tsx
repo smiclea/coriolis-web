f623ff6242ef872c7ed962d87f4e03e66aa7f7b5
Sergiu Miclea <smiclea@cloudbasesolutions.com>
2020-11-13 10:42:46 +0200

    Implement Minion Pool feature redesign
    Minion Pool List
    - status pill updates
    - number of machines created
    
    Minion Pool Details
    - details and status for each machine
    - list of events and product updates
    - new minion pool actions
    
    Minion Pool Popup
    - new fields
    - simple and advanced tabs

--- src/components/pages/MigrationDetailsPage/MigrationDetailsPage.tsx
+++ src/components/pages/MigrationDetailsPage/MigrationDetailsPage.tsx
@@ -318,7 +318,7 @@ class MigrationDetailsPage extends React.Component<Props, State> {
     const dropdownActions = [
       {
         label: 'Cancel',
-        disabled: this.getStatus() !== 'RUNNING',
+        disabled: this.getStatus() !== 'RUNNING' && this.getStatus() !== 'AWAITING_MINION_ALLOCATIONS',
         hidden: this.getStatus() === 'CANCELLING',
         action: () => { this.handleCancelMigrationClick() },
       },
